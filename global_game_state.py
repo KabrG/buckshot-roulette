9ef84da01e7e85084f97b96161432938262b12b9
Tan-ush <147014914+Tan-ush@users.noreply.github.com>
2024-05-01 15:42:00 -0400

    Creating the outline of the Player Class

--- global_game_state.py
+++ global_game_state.py
@@ -1,52 +1,48 @@
 import math
-# testing
-'''
-For items,
-1 - Cigarette
-2 - Cell Phone
-3 - Magnifying Glass
-4 - Cuffs
-5 - Inverter
-6 - Injection
-7 - Beer
-8 - Handsaw
-9 - Medicine
+class Player:
+    """
+    Buckshot Roulette Player Object
 
-'''
-# The game should only have the all-knowing copy
-global_game_state = {
-    "shells" : [], # Should only have 1's and 0's
-    "p1_health" : 0,
-    "p2_health" : 0,
-    "p1_items"  : [],
-    "p2_items"  : [],
-    "p1_cuffed" : False,
-    "p2_cuffed" : False,
+    --- Items ---
+    1 - Cigarette
+    2 - Cell Phone
+    3 - Magnifying Glass
+    4 - Cuffs
+    5 - Inverter
+    6 - Injection
+    7 - Beer
+    8 - Handsaw
+    9 - Medicine
 
-}
+    --- Public Attributes ---
+    shell - List
+    my_health - Int
+    other_health - Int
+    my_items - List[Str]
+    other_items - List[Str]
+    my_cuffed - Bool
+    other_cuffed - Bool
+    turn - Bool
 
-p1_game_state = {
-    "shells": [],  # Can have 0's, 1's, or 2's
-    "p1_health": 0,
-    "p2_health": 0,
-    "p1_items": [],
-    "p2_items": [],
-    "p1_cuffed": False,
-    "p2_cuffed": False,
-}
+    --- Functions ---
+    new_phase: Create a new phase and change player attributes
+    fire: a shell is shot and change player stats
+    use: Use an item that the user inputs
+    check_end
+    """
 
-p2_game_state = {
-    "shells": [],  # Can have 0's, 1's, or 2's
-    "p1_health": 0,
-    "p2_health": 0,
-    "p1_items": [],
-    "p2_items": [],
-    "p1_cuffed": False,
-    "p2_cuffed": False,
-}
+    def __init__(self, health, other_health, turn):
+        self.shell = []
+        self.my_health = health
+        self.other_health = other_health
+        self.my_items = []
+        self.other_items = []
+        self.my_cuffed = False
+        self.other_cuffed = False
+        self.turn = turn
 
+    def fire(self, opponent=True):
+        pass
 
-
-
-
-
+    def use(self, item: str):
+        pass
