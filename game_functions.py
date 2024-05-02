d50e1f2e48959623b1416c535340da409f16ed23
KabrG <kabirguron@gmail.com>
2024-05-02 15:14:24 -0400

    minor injection changes

--- game_functions.py
+++ game_functions.py
@@ -1,37 +1,11 @@
 
+
 '''
     6 - Injection
     7 - Beer
     8 - Handsaw
     9 - Medicine
 '''
-
-def injection(self_items: list, opponent_items: list, steal_item: str) -> None:
-
-    hasInjection = False
-    injection_index = -1
-
-    # Check that injection exists
-    for i in range(len(self_items)):
-        if self_items[i] == "injection":
-            hasInjection = True
-            injection_index = i
-
-    if not hasInjection:
-        raise Exception("User does not have injection")
-
-
-    for i in range(len(opponent_items)):
-        if opponent_items[i] == steal_item:
-            self_items.append(steal_item)
-            opponent_items.pop(i)
-            self_items.pop(injection_index)
-            return
-
-    # Item not found
-    raise Exception(f"Opponent does not have '{steal_item}'.")
-
-
 
 
 
--- global_game_state.py
+++ global_game_state.py
@@ -46,3 +46,52 @@ class Player:
 
     def use(self, item: str):
         pass
+
+
+def injection(self_items: list, opponent_items: list, steal_item: str) -> None:
+
+    found_item = False
+    has_injection = False
+    injection_index = -1
+
+    # Check that injection exists
+    for i in range(len(self_items)):
+        if self_items[i] == "injection":
+            hasInjection = True
+            injection_index = i
+
+    if not has_injection:
+        raise Exception("User does not have injection")
+
+    for i in range(len(opponent_items)):
+        if opponent_items[i] == steal_item:
+            self_items.append(steal_item)
+            opponent_items.pop(i)
+            self_items.pop(injection_index)
+            found_item = True
+
+    # Item not found
+    if not found_item:
+        raise Exception(f"Opponent does not have '{steal_item}'.")
+
+    # Use item
+    if steal_item == "injection":
+        raise Exception("You may not use injection to steal injection.")
+    elif steal_item == "cigarette":
+        pass # Add cigarette function
+    elif steal_item == "cell_phone":
+        pass # Add cell_phone function
+    elif steal_item == "magnifying_glass":
+        pass # Add magnifying_glass function
+    elif steal_item == "cuffs":
+        pass # Add cuffs function
+    elif steal_item == "inverter":
+        pass # Add inverter function
+    elif steal_item == "beer":
+        pass # Add beer function
+    elif steal_item == "handsaw":
+        pass # Add handsaw function
+    elif steal_item == "medicine":
+        pass # Add medicine function
+
+
