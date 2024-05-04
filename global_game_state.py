a30dee80ec013adc901a579804b76866c5c60940
KabrG <kabirguron@gmail.com>
2024-05-03 20:43:01 -0400

    added 5 items

--- global_game_state.py
+++ global_game_state.py
@@ -1,4 +1,5 @@
 import math
+import random
 class Player:
     """
     Buckshot Roulette Player Object
@@ -39,6 +40,7 @@ class Player:
         self.other_items = []
         self.my_cuffed = False
         self.other_cuffed = False
+        self.damage = 1
         self.turn = turn
 
     def fire(self, opponent=True):
@@ -48,26 +50,65 @@ class Player:
         pass
 
 
-def injection(self_items: list, opponent_items: list, steal_item: str) -> None:
+def handsaw(p1: Player):
+    if p1.damage == 2: # handsaw was already used
+        pass
+    elif p1.damage == 1:
+        p1.damage = 2
+    else:
+        raise Exception("Player has a damage attribute that is impossible to obtain")
+
+
+def medicine(p1: Player, max_health):
+    dice_roll = random.randint(0, 100)
+    if dice_roll <= 40 and (p1.my_health + 2) <= max_health:
+        p1.my_health += 2
+    elif (p1.my_health + 2) <= max_health:
+        pass
+    else:
+        p1.my_health -= 1
+
+
+def beer(p1: Player, shell_list, shell_index) -> None:
+    has_beer = False
+
+    # Check that player has beer
+    for i in range(len(p1.my_items)):
+        if p1.my_items[i] == "beer":
+            has_beer = True
+            beer_index = i
+
+    # User doesn't have item
+    if not has_beer:
+        raise Exception("User does not have injection")
+
+    # Reveal next shell
+    p1.shell[shell_index] = shell_list[shell_index]
+    # How should p2 know?
+
+
+
+def injection(p1: Player, p2: Player, steal_item: str) -> None:
 
     found_item = False
     has_injection = False
     injection_index = -1
 
     # Check that injection exists
-    for i in range(len(self_items)):
-        if self_items[i] == "injection":
-            hasInjection = True
+    for i in range(len(p1.my_items)):
+        if p1.my_items[i] == "injection":
+            has_injection = True
             injection_index = i
 
     if not has_injection:
         raise Exception("User does not have injection")
 
-    for i in range(len(opponent_items)):
-        if opponent_items[i] == steal_item:
-            self_items.append(steal_item)
-            opponent_items.pop(i)
-            self_items.pop(injection_index)
+    # Check if the opponent has the item
+    for i in range(len(p2.my_items)):
+        if p2.my_items[i] == steal_item:
+            p2.my_items.pop(i)
+            p1.my_items.pop(injection_index)
+            p1.my_items.append(steal_item)
             found_item = True
 
     # Item not found
@@ -93,5 +134,51 @@ def injection(self_items: list, opponent_items: list, steal_item: str) -> None:
         pass # Add handsaw function
     elif steal_item == "medicine":
         pass # Add medicine function
+
+def initialize_game(p1: Player, p2: Player, shell):
+    total_shell = random.randint(2, 8)
+    shell = []
+    shell[:] = [2]*total_shell
+
+    # at least ~33% live, ~33% dud
+    min_live = math.ceil(0.34 * total_shell)
+    min_dud = math.ceil(0.34 * total_shell)
+    shell[:min_live] = [1] * min_live
+    shell[min_live:-1] = [0] * min_dud
+
+    for i in range(len(shell)):
+        if shell[i] == 2:
+            shell[i] = random.randint(0, 1)
+
+    # Shuffle list
+    random.shuffle(shell)
+
+    # Assign Max Health
+    max_health = random.randint(2, 6)
+    p1.my_health = max_health
+    p2.my_health = max_health
+
+    # Assign Turn
+    if random.randint(0, 1) == 0:
+        p1.turn = True
+        p2.turn = False
+    else:
+        p1.turn = False
+        p2.turn = True
+
+    # Distribute items
+    num_items = random.randint(1, 3) # Come back if needed
+    item_list = ["cigarette", "cell_phone", "magnifying_glass",
+                 "cuffs", "inverter", "injection", "beer", "handsaw", "medicine"]
+
+    for i in range(num_items):
+        p1.my_items.append(item_list[random.randint(0, 8)])
+        p2.my_items.append(item_list[random.randint(0, 8)])
+
+
+
+
+
+
 
 
